62d23db0b3021aba4426f7d33dd097d8799b72ac
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-03 14:34:32 +0200

    move mongo connection

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express"
 import 'dotenv/config'
 import { router as quizRouter } from "./routes/quiz.js"
+import { router as salarieRouter } from "./routes/salarie.js";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://127.0.0.1:27017/quizs')
+    .then(() => {
+        console.log("connected")
+    }).catch(err => console.error(err))
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/quiz', quizRouter)
+app.use('/api/salarie', salarieRouter)
+
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server started on http://localhost:${process.env.SERVER_PORT}`)
--- models/quiz.js
+++ models/quiz.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/quizs')
-    .then(() => {
-        console.log("connected")
-    }).catch(err => console.error(err))
-
 const QuizSchema = new mongoose.Schema({
     note: Number,
     nom: String,
