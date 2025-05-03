77b598513d237b03aa75eba821e0b0e26f544528
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-03 13:53:58 +0200

    #2 basic quiz api

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express"
 import 'dotenv/config'
+import { router as quizRouter } from "./routes/quiz.js"
 
 const app = express()
+app.use(express.json())
+
+app.use('/api/quiz', quizRouter)
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server started on http://localhost:${process.env.SERVER_PORT}`)
