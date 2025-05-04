4967eed071e18139b6c698fcc84e179235766b94
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-04 16:10:39 +0200

    added some model validation

--- models/quiz.js
+++ models/quiz.js
@@ -1,12 +1,42 @@
 import mongoose from "mongoose";
+import { Schema } from "mongoose";
 
-const QuizSchema = new mongoose.Schema({
-    note: Number,
-    nom: String,
-    prenom: String,
-    quizName: String,
-    doneAt: Date,
-    assignedTo: mongoose.Schema.ObjectId,
+const QuizSchema = new Schema({
+    note: {
+        type: Number,
+        min: 0,
+        required: true,
+        immutable:true
+
+    },
+    nom: {
+        type: String,
+        lowercase: true,
+        required: true,
+        match: /^[a-zA-Z\é\ç\è\-]+$/,
+        immutable:true
+    },
+    prenom: {
+        type: String,
+        lowercase: true,
+        required: true,
+        match: /^[a-zA-Zéçè-]+$/,
+        immutable:true
+
+    },
+    quizName: {
+        type: String,
+        required: true,
+        immutable:true
+
+    },
+    doneAt: {
+        type: Date,
+        default:()=> Date.now(),
+        immutable:true
+
+    },
+    assignedTo: { type: Schema.Types.ObjectId, ref: "Salarie" }
 })
 
 export const Quiz = mongoose.model('Quiz', QuizSchema)
--- models/salarie.js
+++ models/salarie.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
 
 const SalarieSchema = new mongoose.Schema({
-    genre: String,
-    nom: String,
-    prenom: String,
-    quizs: [mongoose.Schema.ObjectId]
+    genre: {
+        type: String,
+        enum: ["H", "F"],
+        default: "H",
+        required: true
+    },
+    nom: {
+        type: String,
+        lowercase: true,
+        required: true,
+        match: /^[a-zA-Zéçè-]+$/
+    },
+    prenom: {
+        type: String,
+        lowercase: true,
+        required: true,
+        match: /^[a-zA-Zéçè-]+$/
+    },
+    quizs: { type: [mongoose.Schema.ObjectId], ref: "Quiz" }
 })
 
 export const Salarie = mongoose.model('Salarie', SalarieSchema)
