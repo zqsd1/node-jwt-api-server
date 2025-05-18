42f9123457d24aefcf1be89afe9d4a64769a7dbb
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-18 18:21:38 +0200

    remove les quizs quand on remove le salarie

--- models/salarie.js
+++ models/salarie.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Quiz } from "./quiz.js";
 
 const SalarieSchema = new mongoose.Schema({
     genre: {
@@ -20,6 +21,19 @@ const SalarieSchema = new mongoose.Schema({
         match: /^[a-zA-Zéçè-]+$/
     },
     quizs: { type: [mongoose.Schema.ObjectId], ref: "Quiz" }
+})
+
+SalarieSchema.post('deleteOne', function (doc) {
+})
+
+//quand on delete un salarie faut delete les quiz associé
+SalarieSchema.pre('deleteOne', async function (next) {
+
+    console.log("salarie delete")
+    const salarie = await Salarie.findById(this._conditions._id)
+    if (salarie.quizs)
+        await Quiz.deleteMany({ _id: { $in: salarie.quizs } })
+    next()
 })
 
 export const Salarie = mongoose.model('Salarie', SalarieSchema)
