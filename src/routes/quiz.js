0e043f87f5e7ca2d5d930c74d7d264f18c63a5b3
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-08-03 16:23:43 +0200

    add auth for quiz routes

--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -1,11 +1,17 @@
 import express from "express"
 import { addQuiz, deleteQuiz, getQuiz, listQuiz, quizCounter } from "../controllers/quiz.js"
+import { authenticate } from "../middlewares/auth.js"
 
 export const router = express.Router()
 
+router.use(authenticate)
+
 router.get('/counter', quizCounter)
+
 router.get('/', listQuiz)
+
 router.get('/:id', getQuiz)
+
 router.post('/', addQuiz)
 // router.put('/:id', updateQuiz)
 
